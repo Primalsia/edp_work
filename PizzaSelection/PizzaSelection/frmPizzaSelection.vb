29b5719d41b5390f13728b60ca8371b279c984a7
Aziah.js <93306237+Primalsia@users.noreply.github.com>
2022-09-25 21:13:05 -0400

    Pizza Selection Added

---
+++ PizzaSelection/PizzaSelection/frmPizzaSelection.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmPizzaSelection
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles lblConfirmation.Click
+
+    End Sub
+
+    Private Sub frmPizzaSelection_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
